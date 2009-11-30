a1ce1b11018af6e646c29341f6e6782609060d73
will <devnull@localhost>
2009-11-30 12:31:46 +0000

    support for displaying edges for variables M G H

--- src/WinDisplay.cpp
+++ src/WinDisplay.cpp
@@ -39,65 +39,131 @@ public:
     img_y=dom*scaley;
 
     imageOut = CImg<>(img_x,img_y,1,3,0);
-    cout << "allocating image" << img_x << " " << img_y << "\n";
+    cout << "allocating image: " << img_x << " " << img_y << "\n";
     
     for (int i=0;i<img_x;i++)
       for(int j=0;j<img_y;j++)
 	imageOut(i,j,1)=200;   //uninitialized
         
-    main_disp=CImgDisplay();
+        main_disp=CImgDisplay();
+	//main_disp.close();
    }
 
   ~WinDisplay(){
-    cout << "calling destructor\n";
+    //    cout << "calling destructor\n";
     //delete imageOut;
     //delete main_disp;
   }
 
   void display(){
-    cout << "display\n";
+
     if (main_disp.is_empty() || main_disp.is_closed())
       main_disp=CImgDisplay(imageOut,"Bitmap",0);
     main_disp.display(imageOut);  
   }
 
   void update(IntVarArray q){
-    cout << "update\n";    
 
     // clean image
     for (int i=0;i<img_x;i++){
       for(int j=0;j<img_y;j++){
-	imageOut(i,j,1)=0;
+	rgb(i,j,0,0,0);
       }
     }
 
-    // for each domain value paint block if it is in the current domain
+    // for each domain value paint green block if it is in the current domain
     for (int i=0;i<q.size();i++){
       for (int j=0;j<domain_size;j++){
 
 	if (q[i].in(j)){
 	  for (int dx=0;dx<scalex;dx++)
 	    for (int dy=0;dy<scaley;dy++)
-	      imageOut(i*scalex+dx,j*scaley+dy,1)=200;
+
+	      rgb(j*scalex+dx,i*scaley+dy,0,200,0);
 	}
       }
 
     }
 
-    cout <<"ok update\n";
+
+    // idea of drawing edges for variable M
+    for (int i=0;i<q.size();i++){
+      for (int j=0;j<domain_size;j++){
+	if (q[i].in(j)){
+	  for (int dx=0;dx<scalex;dx++){	    
+	    for (int k=-1;k<2;k++){
+	      int x,y;
+	      x=j*scalex-dx+scalex/2+k;
+	      y=i*scaley-dx+scalex/2;
+	      if (x>0 && y>0 && x<img_x && y<img_y){
+		rgb(x,y,0,0,255);
+	      }
+	    }	    
+	  }
+	}
+      }
+      
+    }
+
+    // variables G
+    for (int i=0;i<q.size();i++){
+      for (int j=0;j<domain_size;j++){
+	if (q[i].in(j)){
+	  for (int dx=0;dx<scalex;dx++){	    
+	    for (int k=-1;k<2;k++){
+	      int x,y;
+	      x=j*scalex   +scalex/2+k;
+	      y=i*scaley-dx+scalex/2;
+	      if (x>0 && y>0 && x<img_x && y<img_y){
+		rgb(x,y,255,0,0);
+	      }
+	    }	    
+	  }
+	}
+      }      
+    }
+
+    // variables H
+    for (int i=0;i<q.size();i++){
+      for (int j=0;j<domain_size;j++){
+	if (q[i].in(j)){
+	  for (int dx=0;dx<scalex;dx++){	    
+	    for (int k=-1;k<2;k++){
+	      int x,y;
+	      x=j*scalex-dx+scalex/2;
+	      y=i*scaley+   scalex/2+k;
+	      if (x>0 && y>0 && x<img_x && y<img_y){
+		rgb(x,y,100,100,100);
+	      }
+	    }	    
+	  }
+	}
+      }      
+    }
+    
+
+
     display();    
   }
 
   void close(){
-    cout << "close (wait 3 sec)\n";
+    cout << "close (wait 1 sec)\n";
     if (!main_disp.is_empty() && !main_disp.is_closed()) {
-      for (int i=0;i<3;i++){
-	cout << 3 - i << endl;
-	main_disp.wait(1000);
+      for (int i=0;i<10;i++){
+	main_disp.wait(100);
       }
       main_disp.close();
     }
   }
 
+
+  /**
+   * Draws a pixel at x,y with color r g b
+   */
+  void rgb(int x, int y, int r, int g, int b){
+    imageOut(x,y,0)=r;
+    imageOut(x,y,1)=g;
+    imageOut(x,y,2)=b;
+  }
 
  }; 
